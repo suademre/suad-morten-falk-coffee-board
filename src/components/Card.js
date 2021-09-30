35898407191f00bdce005ca6b66c4379d013574f
Suad Emre Umar <suadeumar@gmail.com>
2021-09-30 17:03:45 +0200

    add styled component, add props to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Card() {
+function Card({ text, author }) {
   return (
     <>
-      <div>
-        <div>
-          <p>What is Node.js?</p>
-          <p>Max Muster</p>
-        </div>
-      </div>
+      <StyledCards>
+        <p>{text}</p>
+        <StyledAuthorText>{author}</StyledAuthorText>
+      </StyledCards>
     </>
   )
 }
 
-const styledContainer = styled.div``
+const StyledCards = styled.div`
+  min-width: 375px;
+  max-height: 150px;
+  display: flex;
+  flex-direction: column;
+  margin: 10px 10px;
+  padding: 5px 10px;
+  border: 1px solid black;
+  border-radius: 20px;
+  @media only screen and (min-width: 375px) {
+    min-width: 350px;
+    margin: 5px auto;
+    box-shadow: 0 3px 3px 3px black;
+  }
+`
+
+const StyledAuthorText = styled.p`
+  align-self: flex-end;
+`
 
 export default Card
