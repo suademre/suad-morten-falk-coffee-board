c6f05ac34bbf5d50d8e990916b77d23d4a363f34
Suad Emre Umar <suadeumar@gmail.com>
2021-09-30 14:44:50 +0200

    add card component and packed.json

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Card() {
+  return (
+    <>
+      <div>
+        <div>
+          <p>What is Node.js?</p>
+          <p>Max Muster</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const styledContainer = styled.div``
+
+export default Card
