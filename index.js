a079f07cab0c2804248fd2af2e075b830da83672
Suad Emre Umar <suadeumar@gmail.com>
2021-10-04 11:11:24 +0200

    add env variables

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express')
 const connectDatabase = require('./setupMongo')
 const errorHandler = require('./errorHandler')
 
+require('dotenv').config()
+
 const app = express()
 
-const port = 3000
+const { PORT, MONGODB_URI } = process.env
 
-connectDatabase('mongodb://localhost:27017/lean-coffee-board-21-5')
+connectDatabase(MONGODB_URI)
 
 app.use(express.json())
 
 app.use('/api/cards', require('./routes/cards'))
 
-// Wenn ein Fehler auftritt, geht der Request durch diese
-// Middleware
 app.use(errorHandler)
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
 })
